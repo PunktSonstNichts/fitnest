e3b7ba80e373a9c713b42a753e5685d9e40f32dd
PunktSonstNichts <t.meyer-arlt@ostfalia.de>
2017-05-06 15:11:51 +0200

    init code

---
+++ js/main.js
@@ -0,0 +1,47 @@
+$(document).ready(function () {
+   $(".taskday").click(function () {
+      $(".taskday").removeClass("active");
+      $(this).addClass("active");
+
+      $("*[data-day]").hide();
+      $("*[data-day='" + $(this).attr("data-dayid") + "']").show();
+
+      $(this).attr("data-taskcount", $("*[data-day='" + $(this).attr("data-dayid") + "']").size());
+   });
+
+   $(".task-done").click(function () {
+        $(this).parent(".task").toggleClass("done");
+
+        var f = $("#progressbar").width() / $("#progressbar").parent().width() * 100;
+
+        var total = $(".taskday.active").attr("data-taskcount");
+
+        if($(this).parent(".task").hasClass("done")){
+            $("#progressbar").width((f + ((1 / total) * 100)) + '%');
+        }else{
+            $("#progressbar").width(f - ((1 / total) * 100) + '%');
+        }
+   });
+
+    $('*[href^="#"]').on('click', function(event) {
+        var target = $(this.getAttribute('href'));
+        if( target.length ) {
+            event.preventDefault();
+            $('html, body').stop().animate({
+                scrollTop: target.offset().top
+            }, 1000);
+        }
+    });
+
+    $(window).scroll(function() {
+        var hT = $('#chart_wrapper').offset().top,
+            hH = $('#chart_wrapper').outerHeight(),
+            wH = $(window).height(),
+            wS = $(this).scrollTop();
+        if (wS > (hT+hH-wH)){
+            $(".chartbox").removeClass("hide");
+        }else{
+            $(".chartbox").addClass("hide");
+        }
+    });
+});
